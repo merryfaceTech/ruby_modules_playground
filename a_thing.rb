6720e9634ff605d1204cacafc5323b2fe1ca9c19
Tarik Merrlees <tarik.merrylees@madetech.com>
2022-05-12 14:59:54 +0100

    add files

---
+++ a_thing.rb
@@ -0,0 +1,5 @@
+require_relative './module_example'
+
+puts TestMod::Can_you_see_me
+TestMod.print_stuff
+TestMod::InceptionMod.print_inception
---
+++ module_example.rb
@@ -0,0 +1,13 @@
+module TestMod
+  Can_you_see_me = 10
+
+  def TestMod.print_stuff
+    print "stuff"
+  end
+
+  module InceptionMod
+    def InceptionMod.print_inception
+      print "inception"
+    end
+  end
+end
